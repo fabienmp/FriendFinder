a72c47d2524f7fbd1529f8ae0e704edb518f46c5
Fabien Mansoubi <fabienmp@gmail.com>
2019-06-12 19:42:19 -0700

    added routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const logger = require('./middleware/logger');
+const logger = require('./app/middleware/logger');
 
 app.use(logger);
 
@@ -13,7 +13,11 @@ app.use(express.urlencoded({
 }));
 
 //SET STATIC FOLDER
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app', 'public')));
+
+// ROUTER
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 // SET SERVER LISTENING PORT
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
